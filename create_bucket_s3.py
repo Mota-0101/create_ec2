df0b8585dd5330086a19dbc3d50acb463d6201a1
Mota-0101 <tmota.seg@gmail.com>
2024-08-14 13:41:01 -0300

    Criação de um bucket s3 na AWS, usando Python e Boto3.

---
+++ create_bucket_s3.py
@@ -0,0 +1,12 @@
+import boto3
+
+s3_client = boto3.client('s3')
+
+bucket_name = 'curso-boto3-teste-321'
+
+response = s3_client.create_bucket(
+    Bucket=bucket_name,
+)
+
+print(response)
+print(f'Bucket s3 "{bucket_name}" criado com sucesso.')
